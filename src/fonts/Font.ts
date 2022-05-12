1b1ed92a7fc232fb91ecbbb36c06bba33c86464a
Theophile Avoyne <tavoyne@gmail.com>
2022-05-12 17:06:00 +0200

    Add font folder and bring in Font and FontRegistry

---
+++ src/fonts/Font.ts
@@ -0,0 +1,66 @@
+import fontkit from "@pdf-lib/fontkit";
+import type { Font as FontkitFont } from "@pdf-lib/fontkit";
+
+import type {
+  Font as IFont,
+  FontLocator,
+  FontSource,
+} from "../types/font.d.js";
+
+export default class Font implements IFont {
+  readonly #fontkitFont: FontkitFont;
+  readonly locator: FontLocator;
+
+  constructor(binary: Uint8Array, source: FontSource) {
+    this.#fontkitFont = fontkit.create(binary);
+    this.locator = {
+      fontFamily: source.fontFamily,
+      fontStyle: source.fontStyle,
+      fontWeight: source.fontWeight,
+    };
+  }
+
+  static async create(source: FontSource): Promise<Font> {
+    const response = await fetch(source.url);
+    const binary = new Uint8Array(await response.arrayBuffer());
+    if (
+      typeof window !== "undefined" &&
+      !Array.from(window.document.fonts).find((fontFace) => {
+        return (
+          source.fontFamily === fontFace.family &&
+          source.fontStyle === fontFace.style &&
+          source.fontWeight === parseInt(fontFace.weight, 10)
+        );
+      })
+    ) {
+      const fontFace = new FontFace(source.fontFamily, binary, {
+        style: source.fontStyle,
+        weight: source.fontWeight.toString(),
+      });
+      window.document.fonts.add(fontFace);
+    }
+    return new Font(binary, source);
+  }
+
+  getBaselineShiftAtSize(size: number): number {
+    const scale = size / this.#fontkitFont.unitsPerEm;
+    const { ascent, lineGap } = this.#fontkitFont;
+    return (ascent + lineGap / 2) * scale;
+  }
+
+  getLineHeightAtSize(size: number): number {
+    const scale = size / this.#fontkitFont.unitsPerEm;
+    const { ascent, descent, lineGap } = this.#fontkitFont;
+    return (ascent + Math.abs(descent) + lineGap) * scale;
+  }
+
+  getTextWidthAtSize(text: string, size: number): number {
+    const advanceWidth = this.#fontkitFont
+      .glyphsForString(text)
+      .reduce((acc, glyph) => {
+        return acc + glyph.advanceWidth;
+      }, 0);
+    const scale = size / this.#fontkitFont.unitsPerEm;
+    return advanceWidth * scale;
+  }
+}
---
+++ src/fonts/FontRegistry.ts
@@ -0,0 +1,49 @@
+import Font from "./Font.js";
+
+import type {
+  FontRegistry as IFontRegistry,
+  FontLocator,
+  FontSource,
+} from "../types/font.d.js";
+
+export default class FontRegistry implements IFontRegistry {
+  readonly list: readonly Font[];
+
+  constructor(fonts: readonly Font[]) {
+    this.list = fonts;
+  }
+
+  static async create(sources: readonly FontSource[]): Promise<FontRegistry> {
+    return new FontRegistry(
+      (await Promise.all(
+        sources.map((source) => {
+          return Font.create(source);
+        })
+      )) as readonly Font[]
+    );
+  }
+
+  getFont(query: FontLocator): Font {
+    const font = this.list.find((font) => {
+      return (
+        font.locator.fontFamily === query.fontFamily &&
+        font.locator.fontStyle === query.fontStyle &&
+        font.locator.fontWeight === query.fontWeight
+      );
+    });
+    if (!font) throw new FontNotRegisteredError(query);
+    return font;
+  }
+}
+
+class FontNotRegisteredError extends Error {
+  constructor(locator: FontLocator) {
+    super(
+      [
+        `Font of family \`${locator.fontFamily}\`, weight \``,
+        `${locator.fontWeight}\` and style \`${locator.fontStyle}\` wasn't reg`,
+        "istered.",
+      ].join("")
+    );
+  }
+}
