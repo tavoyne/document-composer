abdcac7da83b9d1ccf4d4151551072b161d8d7ac
Theophile Avoyne <tavoyne@gmail.com>
2022-05-13 17:54:15 +0200

    Implement naive version of placer

--- src/placement/Placer.ts
+++ src/placement/Placer.ts
@@ -1,8 +1,95 @@
-import type { Block } from "../types/blocks.d.js";
-import type { Element } from "../types/elements.d.js";
-import type { Template } from "../types/template.d.js";
+import type { Block } from "../types/blocks.js";
+import type { Element } from "../types/elements.js";
+import type { Placement } from "../types/placement.js";
 
 export default function* placer(
-  blocks: Block,
-  template: Template
-): IterableIterator<Element> {}
+  iterator: IterableIterator<Block>
+): IterableIterator<Element & Placement> {
+  const lookUps: Block[] = [];
+  const marginBottom = 50;
+  const marginTop = 50;
+  const pageHeight = 400;
+  const paperHeight = 500;
+
+  let result = iterator.next();
+
+  let pageIndex = 0;
+  let y = marginTop;
+
+  while (!result.done || lookUps.length) {
+    const block = (lookUps.shift() || result.value!) as Block;
+
+    switch (block.type) {
+      case "absolute": {
+        break;
+      }
+      case "relative": {
+        let groupHeight =
+          block.height + (block.minPresenceAhead && block.spacingBottom);
+        let lookUpIndex = 0;
+        let minPresenceAhead = block.minPresenceAhead;
+
+        while (minPresenceAhead) {
+          let comingBlock: Block | undefined = lookUps[lookUpIndex];
+          lookUpIndex += 1;
+          if (!comingBlock) {
+            comingBlock = iterator.next().value;
+            if (comingBlock) {
+              if (!lookUps.includes(comingBlock)) {
+                lookUps.push(comingBlock);
+              }
+            }
+          }
+          if (comingBlock) {
+            if (comingBlock.type === "relative") {
+              minPresenceAhead = Math.max(
+                minPresenceAhead - 1,
+                block.minPresenceAhead
+              );
+              groupHeight +=
+                comingBlock.height +
+                block.spacingTop +
+                (minPresenceAhead && block.spacingBottom);
+            }
+          } else {
+            minPresenceAhead = 0;
+          }
+        }
+
+        if (groupHeight > pageHeight) {
+          throw new BlockTooTallError();
+        } else if (
+          groupHeight + block.spacingTop + y >
+          paperHeight - marginBottom
+        ) {
+          pageIndex += 1;
+          y = marginTop;
+        } else {
+          y += block.spacingTop;
+        }
+        if (block.element) {
+          yield {
+            ...block.element,
+            height: block.height,
+            heightAhead: 0,
+            heightBehind: 0,
+            pageIndex,
+            y,
+          };
+        }
+        y += block.height + block.spacingBottom;
+        break;
+      }
+    }
+
+    if (!lookUps.length) {
+      result = iterator.next();
+    }
+  }
+}
+
+class BlockTooTallError extends Error {
+  constructor() {
+    super("Block cannot be placed because it's too tall.");
+  }
+}
---
+++ src/placement/placer.test.ts
@@ -0,0 +1,60 @@
+import assert from "node:assert/strict";
+// @ts-ignore
+import test from "node:test";
+
+import { Block } from "../types/blocks.d.js";
+import { Font } from "../types/font.d.js";
+import placer from "./placer.js";
+
+test("Outputs the proper elements out of a minimal block list.", () => {
+  const iterator: IterableIterator<Block> = (function* () {
+    yield {
+      height: 16,
+      label: `0_VIEW_ST`,
+      minPresenceAhead: 1,
+      spacingBottom: 0,
+      spacingTop: 8,
+      type: "relative",
+    };
+    yield {
+      element: {
+        color: [0, 0, 0, 1],
+        font: {} as Font,
+        fontSize: 16,
+        text: "Lorem ipsum dolor sit amet",
+        type: "text",
+        x: 32,
+      },
+      height: 16,
+      label: `1_TEXT_L0`,
+      minPresenceAhead: 1,
+      spacingBottom: 0,
+      spacingTop: 0,
+      type: "relative",
+    };
+    yield {
+      height: 10,
+      label: `0_VIEW_SB`,
+      minPresenceAhead: 1,
+      spacingBottom: 2,
+      spacingTop: 0,
+      type: "relative",
+    };
+  })();
+  const elements = [...placer(iterator)];
+  assert.deepStrictEqual(elements, [
+    {
+      color: [0, 0, 0, 1],
+      font: {},
+      fontSize: 16,
+      height: 16,
+      heightAhead: 0,
+      heightBehind: 0,
+      pageIndex: 0,
+      text: "Lorem ipsum dolor sit amet",
+      type: "text",
+      x: 32,
+      y: 74,
+    },
+  ]);
+});
